5af568d57b1797249155d97f91504e9fdedddd27
Administrator <Administrator@iZ355jbip1wZ>
2018-10-20 12:38:02 +0800

    asdsa

--- order_meal/src/main/java/cn/orderMeal/common/cons/SessionConst.java
+++
@@ -1,5 +0,0 @@
-package cn.orderMeal.common.cons;
-
-public class SessionConst {
-	public static final String WEIXIN_USERINFO = "weixinUserInfo";
-}
--- order_meal/src/main/java/cn/orderMeal/common/kit/session/SessionController.java
+++
@@ -1,7 +0,0 @@
-package cn.orderMeal.common.kit.session;
-
-import com.jfinal.core.Controller;
-
-public abstract class SessionController extends Controller {
-
-}
